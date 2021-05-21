dc92ce992754c2e007ee2773dffb4a10d27e484f
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-05-21 08:04:12 -0300

    feat: create cep controller

---
+++ src/main/java/com/dms/consumer/controller/CepController.java
@@ -0,0 +1,26 @@
+package com.dms.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dms.consumer.model.Address;
+import com.dms.consumer.repository.CepRepository;
+
+@RequestMapping("/cep")
+@RestController
+public class CepController {
+
+	@Autowired
+	private CepRepository cepRepository;
+
+	@GetMapping("/{cep}")
+	public ResponseEntity<Address> getCep(@PathVariable("cep") String cep) {
+		Address address = cepRepository.findForCep(cep);
+
+		return address.isValid() ? ResponseEntity.ok(address) : ResponseEntity.notFound().build();
+	}
+}
