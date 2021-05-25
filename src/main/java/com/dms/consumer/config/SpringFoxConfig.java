dce37ca33a6877f0eb2d645747cf7a4ef2eeb0a9
Diorgenes Morais <diorgenesmorais@gmail.com>
2021-05-25 07:27:48 -0300

    feat: descrever informacoes no swagger

--- src/main/java/com/dms/consumer/config/SpringFoxConfig.java
+++ src/main/java/com/dms/consumer/config/SpringFoxConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,8 +20,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(this.apiInfo())
 				.select()
-					.apis(RequestHandlerSelectors.any())
+					.apis(RequestHandlerSelectors.basePackage("com.dms.consumer.controller"))
 					.build();
 	}
 	
@@ -29,5 +33,14 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 		
 		registry.addResourceHandler("/webjars/**")
 					.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+					.title("Consumer API")
+					.description("API que consome outra API")
+					.version("1.0")
+					.contact(new Contact("DMS software", "https://github.com/diorgenesmorais/consumer", "diorgenesmorais@gmail.com"))
+					.build();
 	}
 }
